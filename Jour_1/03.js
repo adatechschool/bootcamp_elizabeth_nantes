63541fbdd69a67dbddb16188942957f65c9d3012
realalexis <alexismoulliere44@gmail.com>
2024-05-27 15:51:50 +0200

    fix majority

--- Jour_1/03.js
+++ Jour_1/03.js
@@ -1,15 +1,17 @@
 const majority = (arr) => {
   const counts = {};
   let max = 0;
-  let maxElement = null;
+  let maxElement = [];
   let even = 0;
   let odd = 0;
 
-  for (let num in arr) {
+  for (let num of arr) {
     counts[num] = counts[num] ? counts[num] + 1 : 1;
     if (counts[num] > max) {
       max = counts[num];
-      maxElement = num;
+      maxElement = [num];
+    } else if (counts[num] === max) {
+      maxElement.push(num);
     }
     if (num % 2 === 0) {
       even++;
@@ -18,8 +20,8 @@ const majority = (arr) => {
     }
   }
   console.log(max, maxElement, even, odd);
-  if (max > 1) {
-    return maxElement;
+  if (max > 1 && maxElement.length === 1) {
+    return maxElement[0];
   } else if (even > odd) {
     return "even";
   } else if (odd > even) {
@@ -29,4 +31,4 @@ const majority = (arr) => {
   }
 };
 
-console.log(majority([1, 2, 3, 4]));
+console.log(majority([33, 44, 55, 66, 77]));

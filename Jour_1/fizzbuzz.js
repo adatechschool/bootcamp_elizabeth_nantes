ad8f142280f9b9e7ded0d9f123bb0f6500642fab
oudinyannick <yannickoudindu49@gmail.com>
2024-05-27 10:34:25 +0200

    exercice fizzbuzz

---
+++ Jour_1/fizzbuzz.js
@@ -0,0 +1,11 @@
+function count(number) {
+    for (let count= 1; number <= 100; i++) {
+        if (number %3) {
+            console.log("Fizz")
+        } else if (number%5) {
+            console.log("Buzz")
+        } else if (number%15) {
+            console.log(FizzBuzz)
+        }       
+    }
+}

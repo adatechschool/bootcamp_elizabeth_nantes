fab57629fb6b29a424ac81b4058edbf5bf71b67a
Loïck Marin <marin.loick@gmail.com>
2024-05-27 10:53:01 +0200

    premier exercice

---
+++ Jour_1/01_récursivité.js
@@ -0,0 +1,13 @@
+function countWays(n) {
+    
+    if (n == 0) 
+        return 1;
+    if (n < 0) 
+        return 0;
+
+    
+    return countWays(n - 1) + countWays(n - 2) + countWays(n - 3);
+}
+
+
+console.log(countWays(4));

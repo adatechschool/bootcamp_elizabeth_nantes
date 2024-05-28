8cf334cf47b7ff9180ec1ef55d79b6214a45081c
Marie Le Lamer <lelamer.marie17@gmail.com>
2024-05-28 15:45:24 +0200

    ajout exo poo

--- Jour_2/POO.md
+++ Jour_2/POO.md
@@ -1,2 +1,8 @@
 Imagine you have a call center with three levels of employees: respondent, manager, and director. An incoming telephone call must be first allocated to a respondent who is free. If the respondent can't handle the call, he or she must escalate the call to a manager. If the manager is not free or not able to handle it, then the call should be escalated to a director. Design the classes and data structures for this problem. Implement a method dispatchCall() which assigns a call to the first available employee.
 
+-if free : respondent
+-if respondent not free or problem : manager 
+-if manager not free or can't handle : director
+
+dispatchCall() assigned to first available employee
+
---
+++ Jour_2/poo.ts
@@ -0,0 +1,40 @@
+class Employee {
+    name: string;
+    level: string;
+    isAvailable: boolean;
+
+    constructor(name: string, level: string, isAvailable: boolean) {
+        this.name = name;
+        this.level = level;
+        this.isAvailable = isAvailable;
+    }
+
+    takeCall() {
+        this.isAvailable = false;
+    }
+}
+
+const respondant = new Employee("Bernardine", "respondant", true);
+const manager = new Employee("Constantine", "manager", true);
+const director = new Employee("Corinne", "director", true);
+
+function dispatchCall() {
+    let incomingCall = 3;
+    while (incomingCall > 0) {
+        if (respondant.isAvailable) {
+            incomingCall--;
+            respondant.isAvailable = false;
+            console.log("respundant took the call !");
+        } else if (manager.isAvailable) {
+            incomingCall--;
+            manager.isAvailable = false;
+            console.log("manager took the call !");
+        } else if (director.isAvailable) {
+            incomingCall--;
+            director.isAvailable = false;
+            console.log("director is very angry since she took the call !");
+        }
+    }
+}
+
+dispatchCall();

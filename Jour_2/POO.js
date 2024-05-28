95c81d51ba4038e219342f43c18e4921d673e715
Pierre Pedrono <146751236+PierrePedrono@users.noreply.github.com>
2024-05-28 11:34:35 +0200

    toutes classes suite conseils le V

--- Jour_2/POO.js
+++ Jour_2/POO.js
@@ -1,21 +1,53 @@
-class Respondent {
-  constructor(name, isFree = true) {
-    this.name = name;
-    this.isFree = isFree;
-};
+class CallCenter {
+    constructor(name, isFree=true){
+    this.name=name
+    this.isFree
+    }
+
 
     dispatchCall() {
-if (isFree = true){
-    console.log(`${this.name} a pris l'appel`)
+        if (Respondent.isFree = true){
+            Respondent.answerCall()}
+        else {
+            if (Manager.isFree = true){
+                Manager.answerCall()
+            }
+            else  {
+                if (Director.isFree = true){
+                    Director.answerCall()
+                }
+                else console.log("C'est la pause crêpes, merci de nous laisser tranquilles pour le moment.")
+            }
+        }
+        
 }
-    }
- 
 
-  class Manager extends Respondent {
+
+
+
+class Respondent {
     constructor(name, isFree = true) {
       this.name = name;
       this.isFree = isFree;
     }
+  
+    answerCall() {
+      console.log(`${this.name} a pris l'appel`);
+      this.isFree = false;
+    }
+  }
+ 
 
+ 
 
+class Manager extends Respondent {
+    constructor(name, isFree){
+    super(name, isFree)
+    }
+}
+
+class Director extends Respondent {
+    constructor(name, isFree){
+    super(name, isFree)
+    }
 }

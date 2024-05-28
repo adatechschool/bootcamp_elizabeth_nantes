84afc807602c1c08f472313f4577da2077a9e008
EdithMenechi <edith.menechi@gmail.com>
2024-05-28 12:06:39 +0200

    tout bon avec un seul appel

--- Jour_2/POO.js
+++ Jour_2/POO.js
@@ -8,27 +8,64 @@
 //respondent répond au téléphone. si pas dispo -> manager. si pas dispo -> director.
 
 class respondent {
-    constructor(name, isFree=true) {
+    constructor(name, isFree) {
         this.name = name;
         this.isFree = isFree;
     }
-    dispatchCall(){
-    if (isFree = true) {
-        console.log(`${ this.name } prends l'appel` )
-    } else 
-}
+    answerCall() {
+        console.log(`${this.name} prends l'appel.`);
+        this.isFree = false;
+        console.log(`${this.name} n'est plus disponible.`);
+    }
 }
 
 class manager extends respondent {
-    constructor() {
-        super()
+    constructor(name, isFree) {
+        super(name, isFree)
     }
 }
 
 class director extends manager {
-    constructor() {
-        super()
+    constructor(name, isFree) {
+        super(name, isFree)
     }
 }
 
+class callcenter {
+    constructor(respondent, manager, director) {
+        this.respondent = respondent;
+        this.manager = manager;
+        this.director = director;
 
+    }
+    dispatchCall() {
+        if (this.respondent.isFree === true) {
+            console.log(`${this.respondent.name} est disponible pour prendre l'appel.`);
+            this.respondent.answerCall();
+        } else {
+            console.log(`${this.respondent.name} n'est pas disponible pour prendre l'appel.`);
+            console.log(`${this.manager.name} prends le relai.`);
+            if (this.manager.isFree === true) {
+                console.log(`${this.manager.name} est disponible pour prendre l'appel.`);
+                this.manager.answerCall();
+            } else {
+                console.log(`${this.manager.name} n'est pas disponible pour prendre l'appel.`);
+                console.log(`${this.director.name} prends le relai.`);
+                if (this.director.isFree === true) {
+                    console.log(`${this.director.name} est disponible pour prendre l'appel.`);
+                    this.director.answerCall();
+                } else {
+                    console.log(`${this.director.name} n'est pas disponible pour prendre l'appel.`);
+                    console.log(`Personne n'est disponible, veuillez réitérer votre appel ultérieurement.`);
+                }
+            }
+        }
+    }
+}
+    
+let respondent1 = new respondent('Victor', false);
+let manager1 = new manager('Paola', false);
+let director1 = new director('Talula', false);
+let center = new callcenter(respondent1, manager1, director1);
+
+center.dispatchCall();
